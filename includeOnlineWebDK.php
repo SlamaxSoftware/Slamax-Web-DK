f5e5898ab348af9f70d271eede815192476cf2af
Slamax Software <Slamaxsoftware@gmail.com>
2019-05-27 11:56:31 +0200

    Create includeOnlineWebDK.php

---
+++ includeOnlineWebDK.php
@@ -0,0 +1,14 @@
+<?php
+//VERSION: 1.0
+
+
+//INCLUDES THE SLAMAX WEBDK THRUGH AN ONLINE
+// VCURL REQUEST
+
+$slamaKey = 'XXXXXXXXX';
+//Require a free key from our website and 
+//paste inside the variable above
+
+
+
+//PROPER INCLUSION
